446e17e6e2cc1c87f3448fc44eeb6b4d9ac89cf3
silassakwa <silassakwa2016@gmail.com>
2020-07-27 11:37:27 +0300

    added jquery

---
+++ js/jscript.js
@@ -0,0 +1,24 @@
+
+$(document).ready(function(){
+  $(".product-image").click(function(event){
+    $(".hide").slideToggle();
+    $(".show").slideToggle();
+    $(".show").slideToggle();
+  });
+});
+
+$(document).ready(function(){
+  $(".design-image").click(function(event){
+    $(".hide3").slideToggle();
+    $(".show3").slideToggle();
+    $(".show3").slideToggle();
+  });
+});
+
+$(document).ready(function(){
+  $(".develoment-image").click(function(event){
+    $(".hide2").slideToggle();
+    $(".show2").slideToggle();
+    $(".show2").slideToggle();
+  });
+});
